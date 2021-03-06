e9bd22c7cd97bd83ad91e7757a530fc8d53ba88b
Christian Gonzalez <cgonzalez233@yahoo.com>
2021-03-06 12:41:27 -0500

    Need to fix populate

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,27 +1,64 @@
-const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", async (req, res) => {
+  try {
+    const results = await Tag.findAll({});
+    console.log(results);
+    res.json(results);
+  } catch (err) {
+    console.log(err);
+  }
   // find all tags
   // be sure to include its associated Product data
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", async (req, res) => {
+  try {
+    const results = await Tag.findAll({ where: { id: req.params.id } });
+    console.log(results);
+    res.json(results);
+  } catch (err) {
+    console.log(err);
+  }
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
 
-router.post('/', (req, res) => {
+router.post("/", async (req, res) => {
+  try {
+    const results = await Tag.create(req.body);
+    console.log(results);
+    res.json(results);
+  } catch (err) {
+    console.log(err);
+  }
   // create a new tag
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", async (req, res) => {
+  try {
+    const results = await Tag.update(req.body, {
+      where: { id: req.params.id },
+    });
+    console.log(results);
+    res.json(results);
+  } catch (err) {
+    console.log(err);
+  }
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", async (req, res) => {
+  try {
+    const results = await Tag.destroy({ where: { id: req.params.id } });
+    console.log(results);
+    res.json(results);
+  } catch (err) {
+    console.log(err);
+  }
   // delete on tag by its `id` value
 });
 
